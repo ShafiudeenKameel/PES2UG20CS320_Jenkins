b9b5c2ddeb15b004afbaa265a4d30d49954585da
ShafiudeenKameel <112197801+ShafiudeenKameel@users.noreply.github.com>
2023-02-14 20:50:04 +0530

    Create sample.cpp

---
+++ main/sample.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout<< "PES2UG20CS320\n";
+  return 0;
+}
